0870b3d52505649f6c147e6efed537a5f56958df
Deniel Ivor Cruz <ivorcruz19@gmail.com>
2025-07-10 12:38:55 +0800

    UPDATE USERS AND DELETE

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,3 +29,59 @@ export const createUser = async (req, res) => {
     res.status(500).json({ success: false, error: err.message });
   }
 };
+
+export const updateCondition = async (req, res) => {
+  const { id } = req.params;
+  const { condition } = req.body;
+  try {
+    const user = await User.findByPk(id)
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" })
+    }
+
+    const previousCondition = user.condition
+
+    await user.update({condition: condition})
+
+    res.status(200).json({ success: true, message: `User condition updated from ${previousCondition} to ${condition}`, data: user})
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message})
+  }
+}
+
+export const updateInfo = async (req, res) => {
+  const { id } = req.params;
+  const { password, ...dataToUpdate } = req.body; //exlude the password for now
+  try {
+    const user = await User.findByPk(id)
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" })
+    }
+
+    await user.update(dataToUpdate)
+
+    res.status(200).json({ success: true, message: `User's info updated successfully`, data: user})
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message})
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByPk(id)
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" })
+    }
+
+    await user.destroy()
+
+    res.status(200).json({ success: true, message: `Deleted successfully`})
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message})
+  }
+}
+
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getUsers, createUser } from '../controllers/userController.js';
+import { getUsers, createUser, updateCondition, updateInfo, deleteUser } from '../controllers/userController.js';
 import { verifyToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/get', verifyToken, getUsers);
 router.post('/create', createUser);
+router.put('/update/:id/condition', updateCondition);
+router.put('/update/:id/info', updateInfo);
+router.delete('/delete/:id', deleteUser);
 
 export default router;
